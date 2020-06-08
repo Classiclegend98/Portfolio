68e83c707602dc4e8a0dc9ff19f9826692664b34
Samuel Manresa <samuel.manresa@gmail.com>
2020-06-08 11:51:02 +0300

    include contact from with PHP

---
+++ contact-form.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "samuel.manresa@gmail.com";
+    $headers = "From: Samuel Portfolio ".$mailFrom;
+    $txt = "Samuel Portfolio from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend")
+}
