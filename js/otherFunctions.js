9deea4765907b3c1b3e5b0035d677ed82f508bd7
Samuel Manresa <samuel.manresa@gmail.com>
2020-06-08 22:32:29 +0300

    changes

--- js/otherFunctions.js
+++ js/otherFunctions.js
@@ -1,3 +1,5 @@
+/* Scroll to top option appearing when scroll down */
+
 window.addEventListener('scroll', function () {
     let scroll = document.querySelector('.scrollTop');
     scroll.classList.toggle("active", window.scrollY > 500)
@@ -10,45 +12,15 @@ function scrollToTop() {
     })
 }
 
+/* Changing theme */
+
 function toggle() {
     const body = document.querySelector('body');
     body.classList.toggle('light');
 }
 
 
-// function validate() {
-//     var name = document.getElementById("name").value;
-//     var email = document.getElementById("email").value;
-//     var subject = document.getElementById("subject").value;
-//     var message = document.getElementById("message").value;
-//     var errorMessage = document.getElementById("errorMessage");
-
-//     errorMessage.style.padding = "10px";
-
-//     var text;
-//     if (name.length < 3) {
-//         text = "Please Enter valid Name";
-//         errorMessage.innerHTML = text;
-//         return false;
-//     }
-//     if (subject.length < 5) {
-//         text = "Please Enter Correct Subject";
-//         errorMessage.innerHTML = text;
-//         return false;
-//     }
-//     if (email.indexOf("@") == -1 || email.length < 6) {
-//         text = "Please Enter valid Email";
-//         errorMessage.innerHTML = text;
-//         return false;
-//     }
-//     if (message.length <= 100) {
-//         text = "Please Enter More Than 100 Characters";
-//         errorMessage.innerHTML = text;
-//         return false;
-//     }
-//     alert("Form Submitted Successfully!");
-//     return true;
-// }
+/* Redirected form to my personal email address */
 
 $(document).ready(function () {
     $('.submit').click(function (event) {
